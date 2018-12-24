e0c0e0c904e068d7c2f694d247674c7123752c3f
lsl645031854 <645031854@qq.com>
2018-12-24 22:02:00 +0800

    third

--- src/main/java/com/mmall/concurrency/example/atomic/AtomicExample3.java
+++ src/main/java/com/mmall/concurrency/example/atomic/AtomicExample3.java
@@ -1,13 +1,15 @@
-package com.mmall.concurrency.example.count;
+package com.mmall.concurrency.example.atomic;
 
 import com.mmall.concurrency.annoations.ThreadSafe;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 /**
  * @Author: lsl
@@ -16,15 +18,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @Slf4j
 @ThreadSafe
-public class CountExample2 {
+public class AtomicExample3 {
+
+    private static AtomicBoolean isHappened = new AtomicBoolean(false);
     //请求总数
     public static int clientTotal = 5000;
     //同时并发执行的线程数
     public static int threadTotal = 200;
-    //计数
-    public static AtomicInteger count = new AtomicInteger(0);
     public static void main(String[] args) throws Exception{
-        //线程池
         ExecutorService ExecutorService = Executors.newCachedThreadPool();
         final Semaphore semaphore = new Semaphore(threadTotal);
         final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
@@ -32,7 +33,7 @@ public class CountExample2 {
             ExecutorService.execute(() -> {
                 try {
                     semaphore.acquire();
-                    add();
+                    test();
                     semaphore.release();
                 } catch (Exception e) {
                     log.error("exception",e);
@@ -42,10 +43,12 @@ public class CountExample2 {
         }
         countDownLatch.await();
         ExecutorService.shutdown();
-        log.info("count:{}",count.get());
+        log.info("isHappened:{}",isHappened.get());
     }
 
-    private static void add(){
-        count.incrementAndGet();
+    public static void test(){
+        if (isHappened.compareAndSet(false,true)){
+             log.info("execute");
+        }
     }
 }
